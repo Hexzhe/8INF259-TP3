12e588be93982a266e1ca796ed2b1146b733f0bf
Hexzhe <hexzhe@jsngbt.com>
2019-04-07 16:15:07 -0400

    New project

--- 8INF259-TP3/8INF259-TP3.cpp
+++ 8INF259-TP3/8INF259-TP3.cpp
@@ -1,21 +1,15 @@
-// 8INF259-TP3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/******************************************************************
+* Titre: Vitesse spatiale (8INF259\TP3)
+* Auteur(e)s: Dominique Boivin (BOID31609701)
+*             Jason Gilbert (GILJ05069703)
+* Date: 2019-04-30
+* Description:
+*******************************************************************/
 
 #include "pch.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    //TODO
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
